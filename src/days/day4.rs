ecad2467e43b5e1cd948f3773f2fffd8c84f5ca2
Patrick Fisher <patrick.fisher9999@gmail.com>
2022-12-04 11:21:47 -0600

    cleanup

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,9 +1,8 @@
-use std::collections::{HashMap, HashSet};
 use crate::days::util;
 
-pub(crate) fn part1 () {
+pub(crate) fn part1() {
     println!("Day 4 Part 1");
-    if let Ok(lines) = util::read_lines("resources\\in4.txt" ) {
+    if let Ok(lines) = util::read_lines("resources\\in4.txt") {
         let mut contained_pairs = 0;
         for line in lines {
             if let Ok(ip) = line {
@@ -11,12 +10,20 @@ pub(crate) fn part1 () {
                 let (elf1, elf2) = (split.next().unwrap(), split.next().unwrap());
 
                 let mut split = elf1.split('-');
-                let (elf1_range_start, elf1_range_end) = (split.next().unwrap().parse::<i32>().unwrap(), split.next().unwrap().parse::<i32>().unwrap());
+                let (elf1_range_start, elf1_range_end) = (
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                );
 
                 let mut split = elf2.split('-');
-                let (elf2_range_start, elf2_range_end) = (split.next().unwrap().parse::<i32>().unwrap(), split.next().unwrap().parse::<i32>().unwrap());
-                
-                if elf1_range_start <= elf2_range_start && elf1_range_end >= elf2_range_end || elf2_range_start <= elf1_range_start && elf2_range_end >= elf1_range_end {
+                let (elf2_range_start, elf2_range_end) = (
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                );
+
+                if elf1_range_start <= elf2_range_start && elf1_range_end >= elf2_range_end
+                    || elf2_range_start <= elf1_range_start && elf2_range_end >= elf1_range_end
+                {
                     contained_pairs += 1;
                 }
             }
@@ -27,7 +34,7 @@ pub(crate) fn part1 () {
 
 pub(crate) fn part2() {
     println!("Day 4 Part 2");
-    if let Ok(lines) = util::read_lines("resources\\in4.txt" ) {
+    if let Ok(lines) = util::read_lines("resources\\in4.txt") {
         let mut overlapping_pairs = 0;
         for line in lines {
             if let Ok(ip) = line {
@@ -35,15 +42,21 @@ pub(crate) fn part2() {
                 let (elf1, elf2) = (split.next().unwrap(), split.next().unwrap());
 
                 let mut split = elf1.split('-');
-                let (elf1_range_start, elf1_range_end) = (split.next().unwrap().parse::<i32>().unwrap(), split.next().unwrap().parse::<i32>().unwrap());
+                let (elf1_range_start, elf1_range_end) = (
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                );
 
                 let mut split = elf2.split('-');
-                let (elf2_range_start, elf2_range_end) = (split.next().unwrap().parse::<i32>().unwrap(), split.next().unwrap().parse::<i32>().unwrap());
+                let (elf2_range_start, elf2_range_end) = (
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                    split.next().unwrap().parse::<i32>().unwrap(),
+                );
 
-                let mut elf1_range = elf1_range_start .. elf1_range_end+1;
+                let mut elf1_range = elf1_range_start..elf1_range_end + 1;
 
-                let mut elf2_range = elf2_range_start .. elf2_range_end+1;
-                
+                let elf2_range = elf2_range_start..elf2_range_end + 1;
+
                 if elf1_range.any(|x| elf2_range.contains(&x)) {
                     overlapping_pairs += 1;
                 }
